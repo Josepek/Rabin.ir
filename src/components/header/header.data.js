c120590335a90a3e13087f6f43f6e13664d06676
Yousef Keyghobadi <usef.keyghobadi@gmail.com>
2023-05-14 18:56:05 +0330

    initial commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    path: 'banner',
+    label: 'خانه',
+  },
+  {
+    path: 'services',
+    label: 'درباره ما',
+  },
+  {
+    path: 'pricing',
+    label: 'تماس با ما',
+  },
+  {
+    path: 'testimonials',
+    label: 'کارگزاری',
+  },
+  {
+    path: 'news',
+    label: 'سبدگردان',
+  },
+];
